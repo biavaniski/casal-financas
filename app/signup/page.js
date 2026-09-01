53d1e270152f662a5aefe13df70501cf58c0e220
biavaniski <biavaniski@gmail.com>
2026-09-01 14:07:06 -0300

    Update page.js

--- app/signup/page.js
+++ app/signup/page.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import Link from "next/link";
-import { signup } from "@/app/actions";
+import { signup } from "@/actions";
 
 export default function SignupPage() {
   const [mode, setMode] = useState("create");
